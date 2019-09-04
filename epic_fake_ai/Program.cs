b5c002c96b3ae00b72927b8b116935296bbcb15a
Sanjay K <sanjaykdragon@gmail.com>
2019-09-04 15:54:28 -0400

    Initial commit

---
+++ epic_fake_ai/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace epic_fake_ai
+{
+    class Program
+    {
+        public static List<int> valid_numbers = new List<int> { };
+        static void Main(string[] args)
+        {
+            int minimum_number = 0;
+            int max_number = 10; //game allows numbers from 0 to 10
+
+            for (int i = minimum_number; i < max_number; i++)
+                valid_numbers.Add(i); //setup possible guesses
+
+            Random rnd = new Random();
+            var possible_nums = valid_numbers;
+
+            while (true)
+            {
+                int guessed_int = possible_nums[rnd.Next(0, possible_nums.Count)];
+                Console.WriteLine("is the number {0}?", guessed_int);
+
+                string response = Console.ReadLine();
+                if (response.ToLower().Substring(0,1) == "y") //as long as first letter is y (can be y, ye, yes), will be accepted
+                {
+                    valid_numbers.Add(guessed_int);
+                    possible_nums = valid_numbers; //reset game basically
+                    Console.WriteLine("added");
+                }
+                else
+                {
+                    possible_nums = possible_nums.Where(x => x != guessed_int).ToList(); // Wow ! cool LINQ in 2019!!!!!!!1111!!1111
+                }
+            }
+        }
+    }
+}
---
+++ epic_fake_ai/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("epic_fake_ai")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("epic_fake_ai")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1cd60c0f-b30e-455c-984f-4e4a408cb38e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
